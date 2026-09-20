e6bdc125c31227efa7c07b377c9ad4de251a13a8
Marcel Dziewanowski <marcelowskid@gmail.com>
2026-09-20 21:00:53 +0200

    Dodanie walidacji pustego pola i Toast

--- app/src/main/java/com/example/zadanie3/MainActivity.java
+++ app/src/main/java/com/example/zadanie3/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -29,6 +30,17 @@ public class MainActivity extends AppCompatActivity {
 
         String name = etName.getText().toString().trim();
 
-        tvResult.setText("Cześć, " + name + "!");
+        if (name.isEmpty()) {
+
+            Toast.makeText(
+                    this,
+                    R.string.error_empty_name,
+                    Toast.LENGTH_SHORT
+            ).show();
+
+            return;
+        }
+
+        tvResult.setText(getString(R.string.greeting, name));
     }
 }
